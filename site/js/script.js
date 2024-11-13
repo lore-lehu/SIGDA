b9d9db0ce8b4719c8ed4efe60008632831dea6e7
lore-lehu <evlorele1@gmail.com>
2024-11-13 16:31:23 -0600

    Add files via upload

---
+++ site/js/script.js
@@ -0,0 +1,50 @@
+// JavaScript Document
+$(document).ready(function (e) {
+	$("#uploadimage").on('submit',(function(e) {
+		e.preventDefault();
+		$("#message").empty(); 
+		$('#loading').show();
+		$.ajax({
+        	url: "subida.php",   	// URL a la que se envía la solicitud
+			type: "POST",      				// Tipo de solicitud que se enviará, llamado como método 
+			data:  new FormData(this), 		// Datos enviados al servidor 
+			contentType: false,       		// El tipo de contenido utilizado al enviar datos al servidor. El valor predeterminado es: "application / x-www-form-urlencoded"
+    	    cache: false,					// Para no poder solicitar que las páginas se almacenen en caché
+			processData:false,  			// Para enviar DOMDocument o archivo de datos no procesados, se establece en falso (es decir, los datos no deben estar en forma de cadena)
+			success: function(data)  		// Una función a ser llamada si la solicitud tiene éxito
+		    {
+			$('#loading').hide();
+			$("#message").html(data);			
+		    }	        
+	   });
+	}));
+
+// Función para previsualizar la imagen
+	$(function() {
+        $("#file").change(function() {
+			$("#message").empty();         // Para eliminar el mensaje de error anterior
+			var file = this.files[0];
+			var imagefile = file.type;
+			var match= ["image/jpeg","image/png","image/jpg"];	
+			if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2])))
+			{
+			$('#previewing').attr('src','../noimage.png');
+			$("#message").html("&lt;p id='error'&gt;Selecciona un archivo de imagen válido&lt;/p&gt;"+"&lt;h4&gt;Nota&lt;/h4&gt;"+"&lt;span id='error_message'&gt;Solo jpeg, jpg y png Tipo de imágenes permitidas&lt;/span&gt;");
+			return false;
+			}
+            else
+			{
+                var reader = new FileReader();	
+                reader.onload = imageIsLoaded;
+                reader.readAsDataURL(this.files[0]);
+            }		
+        });
+    });
+	function imageIsLoaded(e) { 
+		$("#file").css("color","green");
+        $('#image_preview').css("display", "block");
+        $('#previewing').attr('src', e.target.result);
+		//$('#previewing').attr('width', '250px');
+		//$('#previewing').attr('height', '230px');
+	};
+});
